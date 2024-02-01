59b7ab446ee7180c606749b5874da1d66a51521d
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-02-01 21:53:36 +0500

    relations yozildi

---
+++ reletions.sql
@@ -0,0 +1,4 @@
+Alter table manufacturer add primary key(id);
+Alter table product add primary key(id);
+Alter table product_category add primary key(id);
+Alter table product_title add primary key(id);
